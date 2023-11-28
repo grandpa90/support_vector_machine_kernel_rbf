62874272787766948db7c6fe432a730059073545
zakaria darwish <zakaria.safi.darwish@gmail.com>
2023-11-28 10:58:29 +0200

    init project

---
+++ support_vector_machine.py
@@ -0,0 +1,62 @@
+# Logistic Regression
+
+# Importing the libraries
+from matplotlib.colors import ListedColormap
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+# Importing the dataset
+dataset = pd.read_csv('/Users/zakariadarwish/Desktop/support_vector_machine_kernel_rbf/Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the Logistic Regression model on the Training set
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# predicting one result 
+print(classifier.predict(sc.transform([[30,87000]])))
+
+print(f"Y_PRED is: {y_pred}")
+cm = confusion_matrix(y_test, y_pred)
+print(f"CONFUSION_MATRIX is: {cm}")
+
+ac = accuracy_score(y_test, y_pred)
+print(f"ACCURACY_SCORE is: {ac}")
+
+# This code is plotting the decision boundary of the K-Nearest Neighbors (K-NN) classifier on the test
+# set.
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+alpha = 0.75, cmap = ListedColormap(('salmon', 'dodgerblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('salmon', 'dodgerblue'))(i), label = j)
+plt.title('SVM - RBF(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+# plt.sg('knn_visualization.png', format='png', dpi=300)
